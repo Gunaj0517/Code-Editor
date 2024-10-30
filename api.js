12113c79ba6f3df500458761a1724408a8febfd5
Brinda <shivibrinda000@gmail.com>
2024-10-31 00:10:48 +0530

    compiler

--- api.js
+++ api.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 const bodyP = require("body-parser");
-app.use(bodyP.json());
 const pathToStaticFiles = process.env.CODE_EDITOR_PATH
 app.use(express.static(pathToStaticFiles));
 const compiler = require("compilex");
 const options = { stats: true };
 compiler.init(options);
-
+app.use(bodyP.json());
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
@@ -18,9 +17,10 @@ app.post("/compile", function (req, res) {
     var code = req.body.code;
     var input = req.body.input;
     var lang = req.body.lang;
+    console.log("Received language:", lang);
     try {
-        if (lang === "Cpp") {
-            var envData = { OS: "windows", cmd: "g++" }; // uses g++ command to compile
+        if (lang === "c++") {
+            var envData = { OS: "windows", cmd: "g++", options:{timeout:10000} }; // uses g++ command to compile
             if (!input) {
                 compiler.compileCPP(envData, code, function (data) {
                      if(data.output){
@@ -30,18 +30,19 @@ app.post("/compile", function (req, res) {
             res.send({output:"Error"})
         }
                 });
-            } else {
+        } 
+        else {
                 compiler.compileCPPWithInput(envData, code, input, function (data) {
-                     if(data.output){
-            res.send(data);
-        }
-        else{
-            res.send({output:"Error"})
-        }
+                    if(data.output){
+                        res.send(data);
+                    }
+                    else{
+                        res.send({output:"Error"})
+                    }
                 });
             }
         }
-        else if (lang == "Java") {
+        else if (lang == "java") {
             if (!input) {
                 var envData = { OS: "windows" };
                 compiler.compileJava(envData, code, function (data) {
@@ -65,7 +66,7 @@ app.post("/compile", function (req, res) {
                 });
             }
         }
-        else if (lang == "Python") {
+        else if (lang === "python") {
             if (!input) {
                 var envData = { OS: "windows" };
                 compiler.compilePython(envData, code, function (data) {
@@ -93,9 +94,10 @@ app.post("/compile", function (req, res) {
             res.send({ output: "Language not supported" });
         }
     } catch (e) {
-        console.log("error", e);
-        res.send({ output: "error" });
+        console.error("Compilation error:", e);
+        res.send({ output: "An error occurred during compilation." });
     }
+    
 });
 
 app.listen(8000, () => {
