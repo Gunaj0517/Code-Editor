37751b6565150f7f40c055e604b0bf568287059f
ParthTiwari-123 <tiwari.parth60@gmail.com>
2024-11-28 23:58:00 +0530

    haye main marjawa

--- user.php
+++ user.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-
+header('Access-Control-Allow-Origin: http://localhost:8000');
+header('Access-Control-Allow-Credentials: true');
 if (isset($_SESSION['name'])) {
     echo $_SESSION['name'];  // Just send the name as plain text
 } else {
